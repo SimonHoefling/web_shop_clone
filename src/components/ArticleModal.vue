33d055372b28166d50c6aa5449186fec5e3a5d1c
Simon Hoefling <simon@hoeflia.eu>
2023-10-20 16:41:09 +0200

    Added and styled clickable size options to choose from in the modal

--- src/components/ArticleModal.vue
+++ src/components/ArticleModal.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { defineProps, defineEmits } from 'vue';
+import { defineProps, defineEmits, ref } from 'vue';
 
 const { selectedArticle, selectedImageIndex } = defineProps(['selectedArticle', 'selectedImageIndex']);
 const emits = defineEmits(['close']);
@@ -7,16 +7,20 @@ const emits = defineEmits(['close']);
 const closeModal = () => {
   emits('close');
 };
+
+const sizes = ref(selectedArticle.sizes);
+const selectedSize = ref('XS');
+
+const updateSelectedSize = (size: string) => {
+  selectedSize.value = size;
+};
 </script>
 
 <template>
   <div class="modal fixed top-0 left-0 w-full h-full flex items-center justify-center z-50">
-    <div class="modal-content bg-white w-3/4 md:w-1/2 max-h-full overflow-y-auto p-4 relative shadow-xl">
-      <font-awesome-icon
-        @click="closeModal"
-        class="cursor-pointer absolute top-4 right-4 text-black text-2xl"
-        icon="times"
-      ></font-awesome-icon>
+    <div class="modal-content bg-white w-3/4 md:w-1/2 max-h-full overflow-y-auto p-4 relative custom-shadow">
+      <font-awesome-icon @click="closeModal" class="cursor-pointer absolute top-4 right-4 text-black text-2xl"
+        icon="times"></font-awesome-icon>
 
       <div class="flex flex-row mt-12">
         <!-- Left side: Display article image -->
@@ -35,17 +39,41 @@ const closeModal = () => {
           <hr class="my-4">
           <p>Color: {{ selectedArticle.colors.join(', ') }}</p>
           <hr class="my-4">
-          <p>Size:</p>
-          <p>{{ selectedArticle.sizes.join(', ') }}</p>
+          <!-- Size options -->
+          <p>Size: <strong>{{ selectedSize }}</strong></p>
+          <div class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-4 mt-2">
+            <p v-for="size in sizes" :key="size" @click="updateSelectedSize(size)" class="size-badge sm:w-12 md:w-12"
+              :class="{ 'selected-size': size === selectedSize }">
+              {{ size }}
+            </p>
+          </div>
+
         </div>
       </div>
     </div>
   </div>
 </template>
 
-
-
-
-
 <style scoped>
+.size-badge {
+  background-color: #bcc0c3;
+  /* width: 4rem; */
+  border-radius: 99px;
+  color: #000000;
+  /* margin: 10px 10px 0 0;  */
+  text-align: center;
+  cursor: pointer;
+  border: 2px solid transparent;
+}
+
+.selected-size {
+  border: 2px solid #000000;
+  color: #000000;
+  text-align: center;
+  /* Add additional styling for the selected size */
+}
+
+.custom-shadow {
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
 </style>
